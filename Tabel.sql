0a80ab482fe020ac61193553e1f18d324a9f4cf3
11-pog <alvessamuel872@gmail.com>
2025-11-28 08:24:57 -0300

    Tabel

---
+++ Tabel.sql
@@ -0,0 +1,46 @@
+﻿/*
+Script de implantação para Andery
+
+Este código foi gerado por uma ferramenta.
+As alterações feitas nesse arquivo poderão causar comportamento incorreto e serão perdidas se
+o código for gerado novamente.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Andery"
+:setvar DefaultFilePrefix "Andery"
+:setvar DefaultDataPath "C:\Users\Alunos\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+:setvar DefaultLogPath "C:\Users\Alunos\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+
+GO
+:on error exit
+GO
+/*
+Detecta o modo SQLCMD e desabilita a execução do script se o modo SQLCMD não tiver suporte.
+Para reabilitar o script após habilitar o modo SQLCMD, execute o comando a seguir:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'O modo SQLCMD deve ser habilitado para executar esse script com êxito.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+PRINT N'Atualização concluída.';
+
+
+GO
